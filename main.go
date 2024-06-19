ad1e9cb1089ef30ce6973f119d81d5bdc0e5e02c
levi-w-lucy <reachfan169@gmail.com>
2024-06-19 08:55:13 -0500

    Added Process function to new struct, and moved getPriceAfterTax helper function

--- main.go
+++ main.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"math"
+
+	"example.com/priceCalculator/prices"
 )
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, .07, .1, .15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = getPriceAfterTax(taxRate, price)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
-}
-
-func getPriceAfterTax(taxRate float64, initialPrice float64) float64 {
-	return Round(initialPrice*(1+taxRate), 2)
 }
 
 //13.456789, 2
--- prices/prices.go
+++ prices/prices.go
@@ -1,9 +1,20 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	Prices            []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	job.TaxIncludedPrices = make(map[string]float64)
+	for _, price := range job.Prices {
+		job.TaxIncludedPrices[fmt.Sprintf("%.2f", price)] = getPriceAfterTax(job.TaxRate, price)
+	}
+
+	fmt.Println(job.TaxIncludedPrices)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
@@ -11,4 +22,8 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 		Prices:  []float64{10, 20, 30},
 		TaxRate: taxRate,
 	}
+}
+
+func getPriceAfterTax(taxRate float64, initialPrice float64) float64 {
+	return initialPrice * (1 + taxRate)
 }
